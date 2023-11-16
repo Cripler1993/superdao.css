49230396bff88857209c5a98df895231b267b495
Cripler1993 <cripler1993@mail.ru>
2023-11-16 13:42:06 +0300

    new commit

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const modalOpen = document.querySelector("#modalOpen");
 const modalClose = document.querySelector(".modal__close");
 const modalWrapper = document.querySelector(".modal__wrapper");
 
-modalOpen.onclick = function () {
-  modalWrapper.classList.remove("hidden");
-};
+// modalOpen.onclick = function () {
+//   modalWrapper.classList.remove("hidden");
+// };
 
-modalClose.addEventListener("click", function () {
-  modalWrapper.classList.add("hidden");
+modalClose.addEventListener("click", function (event) {
+  if (event.target == modalOpen) {
+    modalWrapper.classList.remove("hidden");
+  } else {
+    modalWrapper.classList.add("hidden");
+  }
 });
 
 // eventlistner. event(на вход), event.target != modalOpen
