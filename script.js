bddcf40aedc6786ef0eead4b28829570d0a3554f
Cripler1993 <cripler1993@mail.ru>
2023-11-20 21:00:58 +0300

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const modalOpen = document.querySelector("#modalOpen");
 const modalClose = document.querySelector(".modal__close");
 const modalWrapper = document.querySelector(".modal__wrapper");
-
-// modalOpen.onclick = function () {
-//   modalWrapper.classList.remove("hidden");
-// };
+const burgerOpen = document.querySelector(".header__burger");
+const burgerClose = document.querySelector(".burger__close");
+const burgerMenu = document.querySelector(".burger__menu");
 
 modalClose.addEventListener("click", function (event) {
   if (event.target == modalOpen) {
@@ -12,6 +11,17 @@ modalClose.addEventListener("click", function (event) {
   } else {
     modalWrapper.classList.add("hidden");
   }
+});
+
+// burgerOpen.onclick = function () {
+//   burgerMenu.classList.remove("hidden");
+// };
+
+burgerOpen.addEventListener("click", function (event) {
+  burgerMenu.classList.remove("hidden");
+});
+burgerClose.addEventListener("click", function (event) {
+  burgerMenu.classList.add("hidden");
 });
 
 // eventlistner. event(на вход), event.target != modalOpen
