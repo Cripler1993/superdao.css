aab1f0fb5200fc95803c890ba39eb0a908439ae8
Cripler1993 <cripler1993@mail.ru>
2023-11-27 22:32:27 +0300

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# superdao.css
+# superdao.css - адаптивная верстка сайта с интерактивными элементами
+
+# ссылка на сайт: https://chipper-lollipop-ec8793.netlify.app/
+
+# Технологии:
+
+# 1. HTML
+
+# 2. CSS
+
+# 3. JavaScript
--- script.js
+++ script.js
@@ -58,15 +58,7 @@ gsap.from(".journey__column p", {
   scrollTrigger: ".journey__column",
   opacity: 0,
 });
-// scrollTrigger после defaults
 
-// const newTimeLine = gsap.timeline({
-//   default: {
-//     duration: 1,
-//     ease: "elastic",
-//   },
-// });
-// newTimeLine
 gsap.from("#works__title", {
   delay: 2,
   duration: 1,
@@ -129,8 +121,68 @@ gsap.from("#works__item-6", {
   scrollTrigger: ".works__with-item",
 });
 gsap.from(".press__item", {
-  duration: 1,
+  duration: 1.5,
   stagger: 0.5,
   scale: 0,
   scrollTrigger: ".press__item",
 });
+// scrollTrigger после defaults
+
+const newTimeLine = gsap.timeline({
+  default: {
+    duration: 1.5,
+  },
+  scrollTrigger: {
+    trigger: "#podcasts__item-1",
+  },
+});
+newTimeLine
+  .from("#podcasts__item-1", {
+    scale: 0,
+  })
+  .from("#podcasts__item-2", {
+    scale: 0,
+  })
+  .from("#podcasts__item-3", {
+    scale: 0,
+  });
+
+const anotherTimeLine = gsap.timeline({
+  default: {
+    duration: 1,
+    ease: "power1.inOut",
+  },
+  scrollTrigger: {
+    trigger: "#project__btn-1",
+  },
+});
+anotherTimeLine
+  .from("#project__btn-1", {
+    delay: 1,
+    opacity: 0,
+    y: "-100%",
+  })
+  .from("#project__btn-2", {
+    opacity: 0,
+    y: "100%",
+  })
+  .from("#project__btn-3", {
+    opacity: 0,
+    y: "-100%",
+  })
+  .from("#project__btn-4", {
+    opacity: 0,
+    y: "100%",
+  })
+  .from("#project__btn-5", {
+    opacity: 0,
+    y: "-100%",
+  })
+  .from("#project__btn-6", {
+    opacity: 0,
+    y: "100%",
+  })
+  .from("#project__btn-7", {
+    opacity: 0,
+    y: "-100%",
+  });
